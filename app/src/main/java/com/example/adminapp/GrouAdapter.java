afd29229ac8723a959e2eac7aca7eb57747968fd
IsuruSG <mgisudeepa99@gmail.com>
2021-09-24 02:21:52 +0530

    Update Process Completed

--- app/src/main/java/com/example/adminapp/GrouAdapter.java
+++ app/src/main/java/com/example/adminapp/GrouAdapter.java
@@ -1,9 +1,14 @@
 package com.example.adminapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,6 +16,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.FirebaseDatabase;
+import com.orhanobut.dialogplus.DialogPlus;
+import com.orhanobut.dialogplus.ViewHolder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -26,8 +39,9 @@ public class GrouAdapter extends FirebaseRecyclerAdapter<GroupsModel,GrouAdapter
         super(options);
     }
 
+
     @Override
-    protected void onBindViewHolder(@NonNull groupHolder holder, int position, @NonNull GroupsModel model) {
+    protected void onBindViewHolder(@NonNull groupHolder holder, final int position, @NonNull GroupsModel model) {
         holder.groupName.setText(model.getName());
         holder.details.setText(model.getDetails());
 
@@ -37,7 +51,63 @@ public class GrouAdapter extends FirebaseRecyclerAdapter<GroupsModel,GrouAdapter
                 .circleCrop()
                 .error(R.drawable.common_google_signin_btn_icon_dark_normal)
                 .into(holder.image);
+
+
+        //Update Process
+        holder.btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final DialogPlus dialogPlus = DialogPlus.newDialog(holder.image.getContext())
+                        .setContentHolder(new ViewHolder(R.layout.update_popup))
+                        .setExpanded(true,1350)
+                        .create();
+
+                View view = dialogPlus.getHolderView();
+
+                EditText name = view.findViewById(R.id.upd_grpName);
+                EditText img = view.findViewById(R.id.upd_grpImage);
+                EditText dtls = view.findViewById(R.id.upd_grpDetails);
+
+                Button btnUpdate = view.findViewById(R.id.btn_update);
+
+                name.setText(model.getName());
+                img.setText(model.getImage());
+                dtls.setText(model.getDetails());
+
+                dialogPlus.show();
+
+                btnUpdate.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Map<String, Object> map = new HashMap<>();
+                        map.put("Name",name.getText().toString());
+                        map.put("Image",img.getText().toString());
+                        map.put("Details",dtls.getText().toString());
+
+                        FirebaseDatabase.getInstance().getReference().child("Groups")
+                                .child(getRef(position).getKey()).updateChildren(map)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        Toast.makeText(holder.groupName.getContext(), "Data Updated Successfully.", Toast.LENGTH_SHORT).show();
+                                        dialogPlus.dismiss();
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(holder.groupName.getContext(), "Error While Updating.", Toast.LENGTH_SHORT).show();
+                                        dialogPlus.dismiss();
+                                    }
+                                });
+                    }
+                });
+            }
+        });
     }
+
+
+
 
     @NonNull
     @Override
@@ -49,8 +119,9 @@ public class GrouAdapter extends FirebaseRecyclerAdapter<GroupsModel,GrouAdapter
     class groupHolder extends RecyclerView.ViewHolder{
 
         CircleImageView image;
-        TextView groupName;
-        TextView details;
+        TextView groupName, details;
+
+        Button btnEdit, btnDelete;
 
         public groupHolder(@NonNull View itemView) {
             super(itemView);
@@ -58,6 +129,9 @@ public class GrouAdapter extends FirebaseRecyclerAdapter<GroupsModel,GrouAdapter
             image = (CircleImageView)itemView.findViewById(R.id.groupImage);
             groupName = (TextView)itemView.findViewById(R.id.groupName);
             details = (TextView)itemView.findViewById(R.id.groupDetails);
+
+            btnEdit = (Button)itemView.findViewById(R.id.btn_groupEdit);
+            btnDelete = (Button)itemView.findViewById(R.id.btn_groupDelete);
         }
     }
 
